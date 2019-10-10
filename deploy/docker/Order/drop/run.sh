30fe5d4739d169a8650631b1d0fcbd6f64136d77
SAMUEL MARTÍNEZ PICAZO <Samuel.Martinez6@alu.uclm.es>
2019-10-10 11:04:54 +0200

    TravisCI ahora crea imágenes con los binarios construidos y los sube a Docker Hub

--- deploy/docker/Order/drop/run.sh
+++ deploy/docker/Order/drop/run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+
+
+
 while ! curl http://mongo:27017/
 do
   echo "$(date) - still trying"
@@ -7,4 +10,4 @@ do
 done
 echo "$(date) - connected successfully"
 
-java -jar ordering-*.jar
+java -jar usr/local/app/ordering-service-0.1.0.jar -spring.config.location=application.properties
